20d0fd45035deade398903b4f4d0d0b003d31b84
Gustas Gedminas <gustas.gedminas@gmail.com>
2024-06-08 20:19:31 +0300

    changed sidebar behaviour

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+const sidebar = document.getElementById("sidebar");
+const hamburgerButton = document.getElementById("hamburger-button");
+
+function openSidebar() {
+  sidebar.classList.remove("-translate-x-full");
+  sidebar.classList.add("translate-x-0");
+}
+
+function closeSidebar() {
+  sidebar.classList.remove("translate-x-0");
+  sidebar.classList.add("-translate-x-full");
+}
+
 function changeActiveLink(e) {
   document.querySelectorAll(".active-link").forEach(element => {
     element.classList.remove("active-link");
@@ -12,9 +25,18 @@ function blurImages() {
   });
 }
 
+hamburgerButton.addEventListener("click", () => {
+  if (sidebar.classList.contains("-translate-x-full")) {
+    openSidebar();
+  } else {
+    closeSidebar();
+  }
+});
+
 document.addEventListener("htmx:beforeRequest", (e) => {
   changeActiveLink(e);
   blurImages();
+  closeSidebar();
 
   document.title = e.detail.elt.textContent;
 });
